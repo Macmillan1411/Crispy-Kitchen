401b1039e6e05f7362ab8dd0a44aa2e8a27ec147
Macmillan1411 <macmillantapiwanashe1411@gmail.com>
2024-08-03 00:15:58 +0300

    image field added to model

--- menu/models.py
+++ menu/models.py
@@ -3,12 +3,19 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
     
+
 class Meal(models.Model):
     name = models.CharField(max_length=100)
-    #image = models.ImageField(upload_to='uploads/meals/')
+    image = models.ImageField(upload_to='uploads/meals/', blank = True)
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name 
 
     
 
