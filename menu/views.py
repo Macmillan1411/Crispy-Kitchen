06c8b3dc01c8e548491449ba37a6ae66c94e128c
Macmillan1411 <macmillantapiwanashe1411@gmail.com>
2024-08-03 23:25:19 +0300

    menu list

--- menu/views.py
+++ menu/views.py
@@ -4,8 +4,10 @@ from .models import Meal,Category
 # Create your views here.
 def menu(request):
     breakfast = Category.objects.get(name = 'breakfast')
+    lunch = Category.objects.get(name = 'lunch')
+    dinner = Category.objects.get(name = 'dinner')
     meals = Meal.objects.filter(category = breakfast)
-    for meal in meals:
-        print(meal)
+    lunch_meals = Meal.objects.filter(category = lunch)
+    dinner_meals = Meal.objects.filter(category = dinner)
     
-    return render(request,'menu/menu.html',{'meals': meals})
+    return render(request,'menu/menu.html',{'meals': meals, 'lunch_meals' : lunch_meals, 'dinner_meals' : dinner_meals})
